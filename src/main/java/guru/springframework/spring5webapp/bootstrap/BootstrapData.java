8c2ad62fc0bab37df8ffd0fa0f38cf41829ca4a8
Shane <shane@cosmictones.org>
2020-09-27 17:55:25 +0100

    add basic jpa entities, repositories and bootstrap

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,50 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repository.AuthorRepository;
+import guru.springframework.spring5webapp.repository.BookRepository;
+import guru.springframework.spring5webapp.repository.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book book = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(book);
+        book.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(book);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "239237");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        Publisher cosmicTonesPublications = new Publisher("Cosmic Tones Publications", "", "", "", "");
+        publisherRepository.save(cosmicTonesPublications);
+
+        System.out.println("Started in bookstrap");
+        System.out.println("Added " + bookRepository.count() + " books and " + publisherRepository.count() + " publishers");
+    }
+}
